151d5fdda90f5df32ae97551dd1ea4509784ffaf
Nick Fialkowski <nikko444@gmail.com>
2017-11-25 22:00:21 -0500

    Fully refactored the project to comply the MVC model; Added function for eliminating same questions in the quiz round

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -12,15 +12,8 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
-    @objc let questionsPerRound = 4
-    @objc var questionsAsked = 0
-    @objc var correctQuestions = 0
-    @objc var indexOfSelectedQuestion: Int = 0
-    @objc var questionDictionary: [String: String] = [:]
-    
-    @objc var gameSound: SystemSoundID = 0
-    
     let triviaProvider = TriviaProvider()
+    var gamePlay = GamePlay()
     
     @IBOutlet weak var questionField: UILabel!
     @IBOutlet weak var trueButton: UIButton!
@@ -30,10 +23,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadGameStartSound()
+        gamePlay = GamePlay(questionField: questionField, trueButton: trueButton, falseButton: falseButton, playAgainButton: playAgainButton)
+        gamePlay.loadGameStartSound()
         // Start game
-        playGameStartSound()
-        displayQuestion()
+        gamePlay.playGameStartSound()
+        gamePlay.displayQuestion()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,84 +35,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @objc func displayQuestion() {
-        questionDictionary = triviaProvider.provide()
-        questionField.text = questionDictionary["Question"]
-        playAgainButton.isHidden = true
-    }
-    
-    @objc func displayScore() {
-        // Hide the answer buttons
-        trueButton.isHidden = true
-        falseButton.isHidden = true
-        
-        // Display play again button
-        playAgainButton.isHidden = false
-        
-        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
-        
-    }
-    
     @IBAction func checkAnswer(_ sender: UIButton) {
-        // Increment the questions asked counter
-        questionsAsked += 1
-   
-        let correctAnswer = questionDictionary["Answer"]
-        
-        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
-            correctQuestions += 1
-            questionField.text = "Correct!"
-        } else {
-            questionField.text = "Sorry, wrong answer!"
-        }
-        
-        loadNextRoundWithDelay(seconds: 2)
+        gamePlay.checkAnswer(sender)
     }
-    
-    @objc func nextRound() {
-        if questionsAsked == questionsPerRound {
-            // Game is over
-            displayScore()
-        } else {
-            // Continue game
-            displayQuestion()
-        }
-    }
-    
+
     @IBAction func playAgain() {
-        // Show the answer buttons
-        trueButton.isHidden = false
-        falseButton.isHidden = false
-        
-        questionsAsked = 0
-        correctQuestions = 0
-        nextRound()
+        gamePlay.playAgain()
     }
-    
 
-    
-    // MARK: Helper Methods
-    
-    @objc func loadNextRoundWithDelay(seconds: Int) {
-        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
-        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
-        // Calculates a time value to execute the method given current time and delay
-        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
-        
-        // Executes the nextRound method at the dispatch time on the main queue
-        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
-            self.nextRound()
-        }
-    }
-    
-    @objc func loadGameStartSound() {
-        let pathToSoundFile = Bundle.main.path(forResource: "GameSound", ofType: "wav")
-        let soundURL = URL(fileURLWithPath: pathToSoundFile!)
-        AudioServicesCreateSystemSoundID(soundURL as CFURL, &gameSound)
-    }
-    
-    @objc func playGameStartSound() {
-        AudioServicesPlaySystemSound(gameSound)
-    }
 }
-
