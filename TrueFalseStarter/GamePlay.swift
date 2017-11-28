7cc84f1a7d53482a83e567707a3005173c309b99
Nick Fialkowski <nikko444@gmail.com>
2017-11-27 21:39:54 -0500

    Experimented with timer but still no luck...

--- TrueFalseStarter/GamePlay.swift
+++ TrueFalseStarter/GamePlay.swift
@@ -87,6 +87,7 @@ var controlButton: UIButton!
         answerField.isHidden = true
         questionField.text = questionDictionary.question
         controlButton.isHidden = true
+        loadNextRoundWithDelay(seconds: 2)
     }
     
     
@@ -156,7 +157,6 @@ var controlButton: UIButton!
         controlButton.setTitle("Next question", for: .normal)
         }
         controlButton.isHidden = false
-        //loadNextRoundWithDelay(seconds: 2)
     }
     
     
@@ -195,14 +195,14 @@ var controlButton: UIButton!
     
     
     func loadNextRoundWithDelay(seconds: Int) {
-        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
-        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
-        // Calculates a time value to execute the method given current time and delay
-        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+            // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+            let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+            // Calculates a time value to execute the method given current time and delay
+            let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
         
-        // Executes the nextRound method at the dispatch time on the main queue
-        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
-            self.nextRound()
+            // Executes the nextRound method at the dispatch time on the main queue
+            DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+                self.nextRound()
         }
     }
     
